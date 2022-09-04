860021d8d9835d57fa64c02855c28c9a7d5dcdf9
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-09-04 11:41:29 +0200

    ejercicio 9

---
+++ 09/calculadora.py
@@ -0,0 +1,16 @@
+# modulo operaciones basicas de una calculadora (suma, resta, multiplicacion y division)
+
+def suma(a, b):
+    return a + b
+
+
+def resta(a, b):
+    return a - b
+
+
+def multiplicacion(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
---
+++ 09/main.py
@@ -0,0 +1,9 @@
+# En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+# Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+
+from calculadora import *
+
+print("Suma: ", suma(2, 3))
+print("Resta: ", resta(2, 3))
+print("Multiplicación: ", multiplicacion(2, 3))
+print("División: ", division(2, 3))
